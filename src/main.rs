b715ea6da2c5418a6b83816b3612aa00e5aba911
jole <jonas@jole.xyz>
2022-07-15 21:48:58 +0200

    subpages for calendar & remarks

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,48 @@ extern crate rocket;
 
 use rocket_dyn_templates::{context, Template};
 
+fn get_classes() -> Vec<String> {
+    vec!["math", "science"]
+        .into_iter()
+        .map(ToString::to_string)
+        .collect()
+}
+
+fn get_remarks() -> Vec<String> {
+    vec!["programmering i norsk timen"]
+        .into_iter()
+        .map(ToString::to_string)
+        .collect()
+}
+
 #[get("/")]
 fn index() -> Template {
     Template::render(
         "index",
         context! {
             title: "Skole boller",
-            classes: ["math", "science"],
-            remarks: ["programmering i norsk timen"]
+            classes: get_classes(),
+            remarks: get_remarks(),
+        },
+    )
+}
+
+#[get("/calendar")]
+fn calendar() -> Template {
+    Template::render(
+        "calendar",
+        context! {
+            classes: get_classes(),
+        },
+    )
+}
+
+#[get("/remarks")]
+fn remarks() -> Template {
+    Template::render(
+        "remarks",
+        context! {
+            remarks: get_remarks(),
         },
     )
 }
@@ -18,6 +52,6 @@ fn index() -> Template {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, calendar, remarks])
         .attach(Template::fairing())
 }
