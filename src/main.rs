64d4bcfa677e4a4614053a5ac6c113f94abb9da4
jole <jonas@jole.xyz>
2022-07-15 21:10:51 +0200

    basic templating

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_dyn_templates::{context, Template};
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hei, verden!"
+fn index() -> Template {
+    Template::render(
+        "index",
+        context! {
+            title: "Skole boller",
+            classes: ["math", "science"],
+            remarks: ["programmering i norsk timen"]
+        },
+    )
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
 }
